eb8d24aafc55c23888dd969e3ace6d1df1937d8a
KFC <kfclts@gmail.com>
2024-07-16 18:07:30 +0800

    Create Product class and Product Service and httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ProductListComponent } from './components/product-list/product-list.com
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductService,
+    provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
